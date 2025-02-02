cdfff60890b2b41367e09ee5453e5b2cf95f7f3f
loyalfriend1109 <danielwalker021109@gmail.com>
2025-02-02 11:47:35 +0100

    Modify the application_system_test.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -7,7 +7,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   Capybara.server_port = 3001
   Capybara.app_host = "http://127.0.0.1:3001"
 
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400] do |options|
+  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ] do |options|
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--headless")
