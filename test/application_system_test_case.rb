aa9e991f23ffd4439bd4427c7c73990d59269738
loyalfriend1109 <danielwalker021109@gmail.com>
2025-02-05 20:15:23 +0100

    Add Active cable feature into Quotes.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,6 +1,9 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  # For linux use this
+  # driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  # For Windows use this
   Capybara.server_host = "127.0.0.1"
   Capybara.server_port = 3001
   Capybara.app_host = "http://127.0.0.1:3001"
